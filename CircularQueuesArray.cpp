d5dd4f10724639879284c89ae11d6f1af05fa78e
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-18 14:29:26 +0700

    menambah case 2 utk remove

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -143,4 +143,10 @@ int main()
             {
                 q.insert();
                 break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
 }
